3681bac0e9a45ce5e1bd1d3bc1ae69bde2de1678
JustOneDeveloper | Benni <78445321+Just1Developer@users.noreply.github.com>
2024-05-31 00:03:11 +0200

    Initial Commit (2)

--- BitvectorChecker/Bitvector.cs
+++ BitvectorChecker/Bitvector.cs
@@ -2,5 +2,47 @@
 
 public class Bitvector
 {
+	private List<int> vector;
 	
+	public Bitvector(string vect)
+	{
+		vector = new();
+		ReadVector(vect);
+	}
+
+	public int Access(int pos)
+	{
+		return vector[pos];
+	}
+
+	// RANK IS EXCLUSIVE!!! (at least it's supposed to be)
+	
+	public int Rank(int num, int pos)
+	{
+		int counter = 0;
+		for (int i = 0; i < Math.Min(pos, vector.Count - 1); i++)
+		{
+			if (vector[i] == num) counter++;
+		}
+		return counter;
+	}
+
+	public int Select(int num, int pos)
+	{
+		int counter = 0;
+		int i = -1;
+		while (counter < pos)
+		{
+			if (vector[++i] == num) counter++;
+		}
+		return i;
+	}
+
+	private void ReadVector(string s)
+	{
+		foreach (char c in s)
+		{
+			vector.Add(c - '0');
+		}
+	}
 }
--- BitvectorChecker/Program.cs
+++ BitvectorChecker/Program.cs
@@ -1,2 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using BitvectorChecker;
+
 Console.WriteLine("Hello, World!");
+
+string[] commands = {
+	"001110110101010111111111",
+	"access 4",         // Correct output
+	"rank 0 10",        // Todo incorrect output: 4
+	"select 1 14",      // Correct output
+	"rank 1 10",        // Correct output
+	"select 0 3",       // Correct output
+	"access 5",         // Correct output
+};
+int _commands = commands.Length - 1;
+
+Bitvector bitvector = new Bitvector(commands[0]);
+for (int i = 0; i < _commands; ++i) {
+	// Todo just next command
+	ProcessCommand(commands[i + 1], bitvector);
+}
+
+void ProcessCommand(string cmd, Bitvector vect)
+{
+	string[] str = cmd.Split(" ");
+	
+	char command = str[0][0];
+	int arg1 = int.Parse(str[1]);
+	int arg2 = str.Length > 2 ? int.Parse(str[2]) : 0;
+
+	switch (command) {
+		case 'a':
+			Console.WriteLine(vect.Access(arg1));
+			return;
+		case 'r':
+			Console.WriteLine(vect.Rank(arg1, arg2));
+			return;
+		case 's':
+			Console.WriteLine(vect.Select(arg1, arg2));
+			return;
+		default:
+			return;
+	}
+}
